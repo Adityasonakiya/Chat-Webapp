db1d8f53d7042445d35d7d1977775bd21cc9a37f
Adityasonakiya <adityasonakiya29@gmail.com>
2022-10-07 10:43:54 +0530

    chat

--- public1/src/pages/Chat.jsx
+++ public1/src/pages/Chat.jsx
@@ -1,40 +1,57 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { allUsersRoute, host } from "../utils/APIRoutes";
 import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
+import ChatContainer from "../components/ChatContainer";
 
 function Chat() {
     const navigate = useNavigate();
     const [contacts, setContacts] = useState([]);
     const [currentUser, setCurrentUser] = useState(undefined)
+    const [currentChat, setCurrentChat] = useState(undefined)
+    const [isLoaded, setIsLoaded] = useState(false);
     useEffect(() => {
-        const fetching = async() =>{
-        if(!localStorage.getItem("chat-app-user")) {
-            navigate("/login");
-        } else {
-            setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")))
-        }}
+        const fetching = async () => {
+            if (!localStorage.getItem("chat-app-user")) {
+                navigate("/login");
+            } else {
+                setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")))
+                setIsLoaded(true);
+            }
+        }
         fetching();
     }, [])
 
     useEffect(() => {
-        const fetch= async() => {if(currentUser) {
-            if(currentUser.isAvatarImageSet) {
-                const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
-                setContacts(data.data);
-            } else {
-                navigate("/setAvatar");
+        const fetch = async () => {
+            if (currentUser) {
+                if (currentUser.isAvatarImageSet) {
+                    const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+                    setContacts(data.data);
+                } else {
+                    navigate("/setAvatar");
+                }
             }
-        }}
+        }
         fetch();
-    },[currentUser]);
+    }, [currentUser]);
+    const handleChatChange = (chat) => {
+        setCurrentChat(chat);
+    }
 
     return (
         <Container>
             <div className="container">
-                <Contacts contacts={contacts} currentUser={currentUser}/>
+                <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+                {   isLoaded && currentChat === undefined ? (
+                        <Welcome currentUser={currentUser} />
+                    ) : (
+                        <ChatContainer currentChat={currentChat} />
+                    )
+                }
             </div>
         </Container>
     );
@@ -59,6 +76,6 @@ const Container = styled.div`
             grid-template-columns:35% 65%; 
         }
     }    
-`  
+`
 
 export default Chat;
